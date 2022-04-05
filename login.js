7462315bf401d9784a735bfe45f60a760d165492
Risabh Rai <rishabhrai_20150@aitpune.edu.in>
2022-04-05 17:31:57 +0530

    reset password

--- login.js
+++ login.js
@@ -1,34 +1,53 @@
-// import required functions 
-import { getAuth, createUserWithEmailAndPassword, sendEmailVerification, signInWithEmailAndPassword, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js'
-import { getFirestore, collection, doc, addDoc, setDoc, query, where, getDoc, getDocs } from 'https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js';
+// import required functions
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	sendEmailVerification,
+	signInWithEmailAndPassword,
+	onAuthStateChanged,
+	GoogleAuthProvider,
+	signInWithPopup,
+	signOut,
+	sendPasswordResetEmail,
+} from "https://www.gstatic.com/firebasejs/9.6.8/firebase-auth.js";
+import {
+	getFirestore,
+	collection,
+	doc,
+	addDoc,
+	setDoc,
+	query,
+	where,
+	getDoc,
+	getDocs,
+} from "https://www.gstatic.com/firebasejs/9.6.8/firebase-firestore.js";
 const auth = getAuth();
 const db = getFirestore();
 const provider = new GoogleAuthProvider();
 
-
-const email = document.querySelector('.email');
-const password = document.querySelector('.password');
-const register = document.querySelector('.register');
-const login = document.querySelector('.login');
-const sOut = document.querySelector('.sign-out');
-const googleLogin = document.querySelector('.google-login');
-email.value = 'risabhrai2001@gmail.com';
-password.value = '123456789';
-
+const email = document.querySelector(".email");
+const password = document.querySelector(".password");
+const register = document.querySelector(".register");
+const login = document.querySelector(".login");
+const sOut = document.querySelector(".sign-out");
+const googleLogin = document.querySelector(".google-login");
+const forgotPassword = document.querySelector(".forgot-password");
+email.value = "risabhrai2001@gmail.com";
+password.value = "123456789";
 
 // grab the form and prevent default behaviour which is sending PHP request
-const form = document.querySelector('.register-form');
+const form = document.querySelector(".register-form");
 form.onsubmit = function (e) {
 	e.preventDefault();
-}
+};
 
 const writeUserData = async (user) => {
-	await setDoc(doc(db, 'users', user.uid), {
+	await setDoc(doc(db, "users", user.uid), {
 		userName: user.displayName,
-		college: '',
-		mail: user.email
+		college: "",
+		mail: user.email,
 	});
-}
+};
 
 const registerUser = () => {
 	const emailValue = email.value;
@@ -37,18 +56,17 @@ const registerUser = () => {
 	createUserWithEmailAndPassword(auth, emailValue, passwordValue)
 		.then((userCredential) => {
 			const user = userCredential.user;
-			console.log('USER', user);
-			sendEmailVerification(auth.currentUser)
-				.then(() => {
-					console.log('VERIFICATION MAIL SENT');
-				});
+			console.log("USER", user);
+			sendEmailVerification(auth.currentUser).then(() => {
+				console.log("VERIFICATION MAIL SENT");
+			});
 		})
 		.catch((error) => {
 			const errorCode = error.code;
 			const errorMessage = error.message;
-			console.log('error message', errorMessage);
-		})
-}
+			console.log("error message", errorMessage);
+		});
+};
 
 const loginUser = () => {
 	const user = auth.currentUser;
@@ -56,23 +74,21 @@ const loginUser = () => {
 		signInWithEmailAndPassword(auth, email.value, password.value)
 			.then((userCredential) => {
 				const user = userCredential.user;
-				console.log('SIGNED IN', user);
-				// grab data from registeration form and pass it as arguments for writing user data 
+				console.log("SIGNED IN", user);
+				// grab data from registeration form and pass it as arguments for writing user data
 				writeUserData(user);
 			})
 			.catch((error) => {
 				const errorCode = error.code;
 				const errorMessage = error.message;
-				console.log('ERROR', errorMessage);
+				console.log("ERROR", errorMessage);
 			});
+	} else {
+		console.log("get email verified");
 	}
-	else {
-		console.log('get email verified');
-	}
-}
+};
 
 const loginByGoogle = () => {
-
 	signInWithPopup(auth, provider)
 		.then((result) => {
 			// This gives you a Google Access Token. You can use it to access the Google API.
@@ -80,11 +96,12 @@ const loginByGoogle = () => {
 			const token = credential.accessToken;
 			// The signed-in user info.
 			const user = result.user;
-			console.log('GOOGLE SIGN IN SUCCESSFUL', user);
+			console.log("GOOGLE SIGN IN SUCCESSFUL", user);
 			// pass required data as arguments to this function to write user data
 			writeUserData(user);
 			// ...
-		}).catch((error) => {
+		})
+		.catch((error) => {
 			// Handle Errors here.
 			const errorCode = error.code;
 			const errorMessage = error.message;
@@ -95,36 +112,51 @@ const loginByGoogle = () => {
 			const credential = GoogleAuthProvider.credentialFromError(error);
 			// ...
 		});
-}
+};
 
 const updateUserData = () => {
-
 	onAuthStateChanged(auth, (user) => {
 		if (user) {
 			// User is signed in, see docs for a list of available properties
 			// https://firebase.google.com/docs/reference/js/firebase.User
 			const uid = user.uid;
-			console.log('ON AUTH STATE CHANGE EVENT FIRED', user);
+			console.log("ON AUTH STATE CHANGE EVENT FIRED", user);
 			// ...
 		} else {
 			// User is signed out
 			// ...
 		}
 	});
-}
+};
 
 const signOutUser = () => {
-	signOut(auth).then(() => {
-		console.log('Sign-out successful');
-	}).catch((error) => {
-		console.log('Sign-out un successful', error);
-		// An error happened.
-	});
-}
+	signOut(auth)
+		.then(() => {
+			console.log("Sign-out successful");
+		})
+		.catch((error) => {
+			console.log("Sign-out un successful", error);
+			// An error happened.
+		});
+};
 
-register.addEventListener('click', registerUser);
-login.addEventListener('click', loginUser);
-googleLogin.addEventListener('click', loginByGoogle);
+const resetPassword = () => {
+	sendPasswordResetEmail(auth, email)
+		.then(() => {
+			// Password reset email sent!
+			// ..
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			// ..
+		});
+};
+
+register.addEventListener("click", registerUser);
+login.addEventListener("click", loginUser);
+forgotPassword.addEventListener("click", resetPassword);
+googleLogin.addEventListener("click", loginByGoogle);
 updateUserData();
 
-sOut.addEventListener('click', signOutUser);
+sOut.addEventListener("click", signOutUser);
