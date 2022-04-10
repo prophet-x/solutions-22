d72e42d0b98ee3197b86d056a0b4be2300a06cb3
prophet-x <kamalmulani777@gmail.com>
2022-04-10 15:53:20 +0530

    landing page mobile nav update

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ window.addEventListener("DOMContentLoaded", function () {
 	const navButton = document.querySelector("#mobile-nav-menu");
 
 	navButton.addEventListener("click", function () {
+		
 		this.parentElement.classList.toggle("--open");
+		document.querySelector('main').classList.toggle("--blur");
+		document.querySelector('footer').classList.toggle("--blur");
+		document.querySelector('body').classList.toggle("--lock-body");
 	});
+
+	
 });
+
+
+
+document.addEventListener('mouseup', function (e) {
+    var container = document.getElementById('mobile-nav-menu');
+
+    if (!container.contains(e.target) && document.querySelector('nav').classList.contains('--open')) {
+		document.querySelector('nav').classList.toggle('--open');
+		document.querySelector('main').classList.toggle("--blur");
+		document.querySelector('footer').classList.toggle("--blur");
+		document.querySelector('body').classList.toggle("--lock-body");
+    }
+}.bind(this));
