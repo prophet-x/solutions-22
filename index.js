97b4cfc5a0786d76a0919aad485b4f48ca2be2fd
prophet-x <kamalmulani777@gmail.com>
2022-04-11 02:03:42 +0530

    authentication

--- index.js
+++ index.js
@@ -24,3 +24,36 @@ document.addEventListener('mouseup', function (e) {
 		document.querySelector('body').classList.toggle("--lock-body");
     }
 }.bind(this));
+
+document.getElementById('nav-link-account').addEventListener('click',function(e){
+	document.getElementById('modal').classList.toggle('hidden');
+	document.getElementById('overlay').classList.toggle('hidden');
+	document.querySelector('body').classList.toggle("--lock-body");
+});
+
+document.getElementById('closeLoginModal').addEventListener('click',function(e){
+	document.getElementById('modal').classList.toggle('hidden');
+	document.getElementById('overlay').classList.toggle('hidden');
+	document.querySelector('body').classList.toggle("--lock-body");
+});
+
+function openSection(evt, cityName) {
+	// Declare all variables
+	var i, tabcontent, tablinks;
+  
+	// Get all elements with class="tabcontent" and hide them
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+  
+	// Get all elements with class="tablinks" and remove the class "active"
+	tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+  
+	// Show the current tab, and add an "active" class to the button that opened the tab
+	document.getElementById(cityName).style.display = "block";
+	evt.currentTarget.className += " active";
+  }
